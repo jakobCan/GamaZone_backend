9106fa84799b0cf15e6b0d8747d208a165381fc4
Jakob Kabranlar <jakob.kabranlar@ibm.com>
2021-06-29 14:04:59 +0200

    adds User class and updates UserController

--- src/main/java/com/example/gamazone_backend/controller/UserController.java
+++ src/main/java/com/example/gamazone_backend/controller/UserController.java
@@ -1,4 +1,36 @@
 package com.example.gamazone_backend.controller;
 
+import com.example.gamazone_backend.model.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @GetMapping()
+    public ArrayList<User> getUsers(){
+        return null;
+    }
+
+    @GetMapping("/{userId}")
+    public User getUser(@PathVariable int userId){
+        return null;
+    }
+
+    @PostMapping
+    public void createUser(@RequestBody User newUser){
+        // create new user
+    }
+
+    @PutMapping("/{userId}")
+    public void updateUser(@PathVariable int userId){
+        // update user
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable int userId){
+        // delete user
+    }
 }

32f6ac8c4706c32826b3ee1d759181a9a955240f
Jakob Kabranlar <jakob.kabranlar@ibm.com>
2021-06-29 11:14:43 +0200

    setup project

---
+++ src/main/java/com/example/gamazone_backend/GamaZoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamazone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamaZoneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamaZoneBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gamazone_backend/GamaZoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamazone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamaZoneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
